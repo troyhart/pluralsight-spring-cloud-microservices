58220540fd9e2a36001d70cc7993d92f8cb8daf3
troyh <troyh@nwri.com>
2016-12-10 11:13:32 -0700

    Completed Configuration Server and Client

--- m2-configclient/src/main/java/pluralsight/demo/RateController.java
+++ m2-configclient/src/main/java/pluralsight/demo/RateController.java
@@ -1,11 +1,13 @@
 package pluralsight.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
 
   @Value("${rate}")
@@ -16,6 +18,9 @@ public class RateController {
   
   @Value("${tollstart}")
   String tollstart;
+  
+  @Value("${connstring}")
+  String connstring;
 
   @RequestMapping("/rate")
   public String getRate(Model m) {
@@ -23,6 +28,7 @@ public class RateController {
     m.addAttribute("rateamount", rate);
     m.addAttribute("lanes", lanecount);
     m.addAttribute("tollstart", tollstart);
+    m.addAttribute("connstring", connstring);
 
 
     // name of the view
