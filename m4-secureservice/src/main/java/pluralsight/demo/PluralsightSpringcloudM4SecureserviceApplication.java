0d19320959981a314b4410c58796af9ca66fb48d
troyh <troyh@nwri.com>
2016-12-10 11:49:57 -0700

    Adding a secure service
    This service will provide toll data to users who can authenticate
    with github.

---
+++ m4-secureservice/src/main/java/pluralsight/demo/PluralsightSpringcloudM4SecureserviceApplication.java
@@ -0,0 +1,58 @@
+package pluralsight.demo;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+public class PluralsightSpringcloudM4SecureserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PluralsightSpringcloudM4SecureserviceApplication.class, args);
+  }
+
+
+  @RequestMapping("/tolldata")
+ // @PreAuthorize("#oauth2.hasScope('toll_read') and hasAuthority('ROLE_OPERATOR')")
+  public ArrayList<TollUsage> getTollData() {
+
+    TollUsage instance1 = new TollUsage("100", "station150", "B65GT1W", "2016-09-30T06:31:22");
+    TollUsage instance2 = new TollUsage("101", "station119", "AHY673B", "2016-09-30T06:32:50");
+    TollUsage instance3 = new TollUsage("102", "station150", "ZN2GP0", "2016-09-30T06:37:01");
+
+    ArrayList<TollUsage> tolls = new ArrayList<>();
+    tolls.add(instance1);
+    tolls.add(instance2);
+    tolls.add(instance3);
+
+    return tolls;
+  }
+
+
+  public class TollUsage {
+
+    public String Id;
+    public String stationId;
+    public String licensePlate;
+    public String timestamp;
+
+    public TollUsage() {
+    }
+
+    public TollUsage(String id, String stationid, String licenseplate, String timestamp) {
+      this.Id = id;
+      this.stationId = stationid;
+      this.licensePlate = licenseplate;
+      this.timestamp = timestamp;
+    }
+
+
+  }
+}
